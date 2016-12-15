0096b12bf2195730ecdc88196aaf7e3b4363a6a2
Sebastian <90scastro@gmail.com>
2016-12-15 13:31:36 +0100

    delete backup dist directories

--- src/app/admin/shared/admin-item-config.module.ts
+++ src/app/admin/shared/admin-item-config.module.ts
@@ -1,3 +1,1 @@
 export { AIType } from './admin-item-config.types';
-export { AdminItemConfig } from './admin-item-config.class';
-export { AdminItemConfigService } from './admin-item-config.service';
